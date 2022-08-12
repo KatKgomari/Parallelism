fd767759802ed40df6a18e7d3c3174affb0c3bd2
katlego kgomari <KGMKAT002@myuct.ac.za>
2022-08-12 05:10:36 +0200

    MeanFilterSerial can blur pictures as expected

--- src/MeanFilterSerial.java
+++ src/MeanFilterSerial.java
@@ -11,7 +11,7 @@ import java.awt.image.Raster;
 import javax.swing.*;   
 import java.io.*;
 import javax.imageio.*;
-import java.util.ArrayList;
+//import java.util.ArrayIndexOutOfBounds; 
 
 public class MeanFilterSerial{
 
@@ -44,8 +44,14 @@ public class MeanFilterSerial{
         
         //try-catch block for our new image
         try{
-            file2 = new File(outputFile);  // Creating file with the name specifies by the user	
-            int dimensions = windowWidth * windowWidth; // What am I using this for again?
+            file2 = new File(outputFile);  // Creating file with the name specifies by the user
+        }
+        catch(Exception e){
+          e.printStackTrace();
+        }
+        
+        	
+        int dimensions = windowWidth * windowWidth; // What am I using this for again?
              
         // Nested for loop to access the diffferent pixels (Aim: to copy these pixels onto the outputImage)
             for (int i = 0; i < width; i++){
@@ -53,13 +59,15 @@ public class MeanFilterSerial{
                     int[] redList = new int[dimensions];
                     int[] greenList = new int[dimensions];
                     int[] blueList = new int[dimensions];
-                    if ((i >= deviation) && (i <= ((height-1)-deviation)) && (j >= deviation) && (j <= ((width-1)-deviation))){ 
+                   // if ((i >= deviation) && (i <= ((height-1)-deviation)) && (j >= deviation) && (j <= ((width-1)-deviation))){ 
+                        
                         int count = 0;
                         int upperM = i + deviation;
                         int upperN = j + deviation;
+                        
                         for(int m = i - deviation; m <= upperM; m++){    // Do i want to have a whole new variable?
                             for(int n = j - deviation; n <= upperN; n++){   // So we are still at the same j and i point.
-                            
+                            try{    
                        // Color color2 = new Color(red,green,blue);
                        //if ( (i%4==0) && (j%2==0))
                        // outputImage.setRGB(i, j, color2.getRGB());    // But I need to be setting at specific points.
@@ -71,17 +79,39 @@ public class MeanFilterSerial{
                                greenList[count] = color.getGreen();
                                System.out.println("count: " + count +" red: " + redList[count] + " green: " + greenList[count] + " blue: " + blueList[count]);                    
                                count++;
-                    
+                               }
+                               
+                               
+                       catch(Exception e){
+                          continue;
+                      } 
                     }
-                }           
-               
+                    
+                } 
+                //Before we go to the next pixel, let us get the average of the array
+                int meanRed = 0;
+                int meanGreen = 0;
+                int meanBlue = 0;
+                
+                for(int k = 0; k < redList.length; k++){
+                    meanRed += redList[k];
+                    meanGreen += greenList[k];
+                    meanBlue += blueList[k];
+                    
+                }
+                meanRed = (int)meanRed/(count + 1);
+                meanGreen = (int)meanGreen/(count + 1);
+                meanBlue = (int)meanBlue/(count+1);
+                          
+                Color color2 = new Color(meanRed, meanGreen, meanBlue);
+                outputImage.setRGB(i,j, color2.getRGB());               
                 }
                 
             }
-        }
+            try{
             ImageIO.write(outputImage, "png", file2); // Putting the omage into a file.         
-       
-        }catch(IOException e){
+            }
+        catch(IOException e){
             e.printStackTrace();
         }
         
